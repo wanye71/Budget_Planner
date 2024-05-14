b21013b10cdaa46551138c6da259e74383399bec
wanye71 <wayne.hatter1971@gmail.com>
2024-05-14 22:05:43 +0000

    added channel, campaign, and buget models. Yet to test this, though. So can not say it is working properly.

--- budgetplanner-project/clients/admin.py
+++ budgetplanner-project/clients/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Client
+from .models import Client, Campaign, BudgetAllocation, Channel
 
 admin.site.register(Client)
+admin.site.register(Campaign)
+admin.site.register(BudgetAllocation)
+admin.site.register(Channel)
--- budgetplanner-project/clients/models.py
+++ budgetplanner-project/clients/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Client(models.Model):
     client_name = models.CharField(max_length=100)
@@ -12,3 +14,34 @@ class Client(models.Model):
 
     def __str__(self):
         return self.client_name
+    
+class Campaign(models.Model):
+    campaign_name = models.CharField(max_length=100)
+    client_name = models.ForeignKey(Client, on_delete=models.CASCADE)
+    budget_amount = models.DecimalField(max_digits=100, decimal_places=2)  # Add a budget amount field
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.campaign_name
+
+class BudgetAllocation(models.Model):
+    client_name = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='budget_allocations_associated')
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    budget_amount = models.DecimalField(max_digits=100, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    @property
+    def remaining_budget(self):
+        campaigns_total_budget = self.campaigns.aggregate(total_budget=models.Sum('budget_amount'))['total_budget']
+        return self.budget_amount - (campaigns_total_budget or 0)
+
+@receiver(post_save, sender=Campaign)
+def update_budget_allocation(sender, instance, created, **kwargs):
+    if created:
+        budget_allocation = instance.client_name.budget_allocations_associated.get(channel=instance.channel)
+        budget_allocation.budget_amount -= instance.budget_amount
+        budget_allocation.save()
+    
