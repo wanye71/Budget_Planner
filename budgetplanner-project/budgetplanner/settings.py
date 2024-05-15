c1afeea7b3792feaafd6faa7ca216a96293beea5
wayne71 <wayne.hatter1971@gmail.com>
2024-05-15 14:08:38 -0500

    included static files

--- budgetplanner-project/budgetplanner/settings.py
+++ budgetplanner-project/budgetplanner/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+# Directory for global static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "budgetplanner", "static")]
+
+# Directory where static files will be collected to
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
