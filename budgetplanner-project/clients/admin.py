fd10d8baea0120001a546c8870d159b63fcb55ca
wayne71 <wayne.hatter1971@gmail.com>
2024-05-14 20:21:54 -0500

    set list_view columns for BudgetAllocation

--- budgetplanner-project/clients/admin.py
+++ budgetplanner-project/clients/admin.py
@@ -3,5 +3,9 @@ from .models import Client, Campaign, BudgetAllocation, Channel
 
 admin.site.register(Client)
 admin.site.register(Campaign)
-admin.site.register(BudgetAllocation)
+
+class BudgetAllocationAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'budget_amount', 'start_date', 'end_date')
+
+admin.site.register(BudgetAllocation, BudgetAllocationAdmin)
 admin.site.register(Channel)
